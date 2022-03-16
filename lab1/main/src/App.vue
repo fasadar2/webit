ffb46e49906f08e798cae26e227a941141ce8855
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2022-03-16 15:43:19 +0300

    qddqd

---
+++ lab1/main/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id = "app">
+    <Fio lastName = 'Орлов' firstName = 'Максим' middleName = 'Андреевич' > </Fio>
+    <Vyatsu :results="results"></Vyatsu>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+import { Component } from 'vue-property-decorator';
+import Fio from '../../fio/src/components/Fio.vue';
+import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
+
+// const url = 'https://new.vyatsu.ru/';
+// const url = 'http://127.0.0.1:5500/main/src/components/8.Html';
+
+const url = 'http://localhost:8081/admission/';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Fio,
+    Vyatsu,
+  },
+  data() {
+    return { Fio, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response: any) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error: any) => console.log(error));
+  },
+});
+</script>
