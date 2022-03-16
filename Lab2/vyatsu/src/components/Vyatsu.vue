9d7b7d527b50fae58ea4d5058f0b5dadace1e3c0
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2022-03-16 16:33:06 +0300

    iddqd

---
+++ Lab2/vyatsu/src/components/Vyatsu.vue
@@ -0,0 +1,19 @@
+<template>
+  <iframe :srcdoc="results" class="frame" id="frame"></iframe>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    results: [],
+  },
+};
+</script>
+
+<style>
+.frame {
+  width: 1200px;
+  height: 500px;
+  font-size: 32px;
+}
+</style>
