3d92473267a259bf173ec04dd77969c3efd92a39
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2022-03-16 16:17:18 +0300

    first commit

---
+++ Lab2/fio/src/components/Fio.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ lastName }} {{ firstName }} {{ middleName }}</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Fio',
+  props: {
+    lastName: String,
+    firstName: String,
+    middleName: String,
+  },
+});
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
---
+++ Lab2/main/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id = "app">
+    <Fio lastName = 'Орлов' firstName = 'Максим' middleName = 'Андреевич' > </Fio>
+    <Vyatsu :results="results"></Vyatsu>
+    <Login></Login>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+import { Component } from 'vue-property-decorator';
+import Fio from '../../fio/src/components/Fio.vue';
+import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
+import Login from '../../login/src/components/login.vue';
+
+// const url = 'https://new.vyatsu.ru/';
+// const url = 'http://127.0.0.1:5500/main/src/components/8.Html';
+
+const url = 'http://localhost:8084/admission/';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Fio,
+    Vyatsu,
+    Login,
+  },
+  data() {
+    return { Fio, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response: any) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error: any) => console.log(error));
+  },
+});
+</script>
