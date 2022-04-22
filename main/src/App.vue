939119be8a84662bee129be4021b6b67418578b5
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2022-04-22 20:03:43 +0300

    update

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id = "app">
-  <Head></Head>
+    <Fio lastName = 'Орлов' firstName = 'Максим' middleName = 'Андреевич' > </Fio>
+    <Vyatsu :results="results"></Vyatsu>
+    <Register></Register>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Head from '../../head/src/components/head.vue';
+import axios from 'axios';
+import { Component } from 'vue-property-decorator';
+import Fio from '../../fio/src/components/Fio.vue';
+import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
+import Register from '../../register/src/components/register.vue';
+
+// const url = 'https://new.vyatsu.ru/';
+// const url = 'http://127.0.0.1:5500/main/src/components/8.Html';
+
+const url = 'http://localhost:8081/admission/';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    Head,
+    Fio,
+    Vyatsu,
+    Register,
+  },
+  data() {
+    return { Fio, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response: any) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error: any) => console.log(error));
   },
 });
 </script>
--- register/src/components/register.vue
+++ register/src/components/register.vue
@@ -18,10 +18,94 @@
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import axios from 'axios';
 
 export default Vue.extend({
   name: 'Register',
   props: {
+  },
+  methods: {
+    toggleStyleEnter() {
+      const enter :HTMLDivElement = document.querySelector('.autorization_block');
+      const reg :HTMLDivElement = document.querySelector('.registrationtion_block');
+      const enterBtn :HTMLDivElement = document.querySelector('.EnterBtn');
+      const regBtn :HTMLDivElement = document.querySelector('.RegBtn');
+      enter.style.display = 'flex';
+      reg.style.display = 'none';
+      enterBtn.style.color = 'green';
+      regBtn.style.color = 'black';
+    },
+    toggleStyleRegister() {
+      const enter :HTMLDivElement = document.querySelector('.autorization_block');
+      const reg :HTMLDivElement = document.querySelector('.registrationtion_block');
+      const enterBtn :HTMLDivElement = document.querySelector('.EnterBtn');
+      const regBtn :HTMLDivElement = document.querySelector('.RegBtn');
+      enter.style.display = 'none';
+      reg.style.display = 'flex';
+      enterBtn.style.color = 'black';
+      regBtn.style.color = 'green';
+    },
+    signIn() {
+      const log :HTMLInputElement = document.getElementById('log') as HTMLInputElement;
+      const password :HTMLInputElement = document.getElementById('password') as HTMLInputElement;
+      const config = {
+        url: 'https://b83c67c6-f6e2-44a3-b933-0dbe68ae85c0.mock.pstmn.io/auth/login',
+      };
+      const data = {
+        login: log.value,
+        password: password.value,
+      };
+      axios.post(config.url, data, { headers: { 'x-mock-match-request-body': true } })
+        .then((response) => {
+          console.log(response.data.completed);
+          if (response.data.completed) {
+            alert('Успешно!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert('Неверный логин или пароль!');
+        });
+    },
+    signOn() {
+      const regLog :HTMLInputElement = document.getElementById('regLog') as HTMLInputElement;
+      const regPass :HTMLInputElement = document.getElementById('regPass') as HTMLInputElement;
+      const regPass2 :HTMLInputElement = document.getElementById('regPass2') as HTMLInputElement;
+      const regEmail :HTMLInputElement = document.getElementById('regEmail') as HTMLInputElement;
+      const config = {
+        url: 'https://b83c67c6-f6e2-44a3-b933-0dbe68ae85c0.mock.pstmn.io/auth/check',
+      };
+      const data = {
+        login: regLog.value,
+      };
+      if (regEmail.value === '') {
+        alert('Введите почту!');
+        return;
+      }
+      if (regLog.value === '') {
+        alert('Введите логин!');
+        return;
+      }
+      if (regPass.value === '') {
+        alert('Введите пароль!');
+        return;
+      }
+      if (regPass.value !== regPass2.value) {
+        alert('Пароли не совпадают!');
+        return;
+      }
+      axios.post(config.url, data, { headers: { 'x-mock-match-request-body': true } })
+        .then((response) => {
+          console.log(response.data.isValid);
+          if (response.data.isValid) {
+            alert('Логин занят!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          alert('Успешно!');
+        });
+    },
   },
 });
 </script>
